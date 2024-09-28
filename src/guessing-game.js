960490c05e71a60f7ad078f4a047a8976867f9e3
ntsh <106670334+tffl@users.noreply.github.com>
2024-09-28 16:36:01 +0300

    feat: implement guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+	number = 0;
+	min = 0;
 
-    setRange(min, max) {
+	constructor() {
+		return this;
+	}
 
-    }
+	setRange(min, max) {
+		this.min = min;
+		this.max = max;
+	}
 
-    guess() {
+	guess() {
+		return Math.ceil((this.min + this.max) / 2);
+	}
 
-    }
+	lower() {
+		this.max = Math.ceil((this.min + this.max) / 2);
+	}
 
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+	greater() {
+		this.min = Math.ceil((this.min + this.max) / 2);
+	}
 }
 
 module.exports = GuessingGame;
